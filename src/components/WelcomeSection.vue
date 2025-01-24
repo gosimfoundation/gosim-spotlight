cb0b88430a0f58464bd5b5a4d346c20482062e1a
Deniz B. <51525430+dbrlk@users.noreply.github.com>
2025-01-24 20:59:44 +0300

    Update WelcomeSection.vue

--- src/components/WelcomeSection.vue
+++ src/components/WelcomeSection.vue
@@ -57,7 +57,7 @@
         <div class="pt-16 lg:row-span-2 lg:-mr-16 xl:mr-auto">
           <div class="-mx-8 grid grid-cols-2 gap-4 sm:-mx-16 sm:grid-cols-4 lg:mx-0 lg:grid-cols-2 lg:gap-4 xl:gap-8">
             <div class="aspect-square overflow-hidden rounded-xl shadow-xl outline outline-1 -outline-offset-1 outline-black/10">
-              <img alt="" src="https://images.unsplash.com/photo-1590650516494-0c8e4a4dd67e?&auto=format&fit=crop&crop=center&w=560&h=560&q=90" class="block size-full object-cover" />
+              <img alt="" src="@/assets/why1.png" class="block size-full object-cover" />
             </div>
             <div class="-mt-8 aspect-square overflow-hidden rounded-xl shadow-xl outline outline-1 -outline-offset-1 outline-black/10 lg:-mt-40">
               <img alt="" src="https://images.unsplash.com/photo-1557804506-669a67965ba0?&auto=format&fit=crop&crop=left&w=560&h=560&q=90" class="block size-full object-cover" />
@@ -66,7 +66,7 @@
               <img alt="" src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?&auto=format&fit=crop&crop=left&w=560&h=560&q=90" class="block size-full object-cover" />
             </div>
             <div class="-mt-8 aspect-square overflow-hidden rounded-xl shadow-xl outline outline-1 -outline-offset-1 outline-black/10 lg:-mt-40">
-              <img alt="" src="https://images.unsplash.com/photo-1598257006458-087169a1f08d?&auto=format&fit=crop&crop=center&w=560&h=560&q=90" class="block size-full object-cover" />
+              <img alt="" src="@/assets/why2.png" class="block size-full object-cover" />
             </div>
           </div>
         </div>
