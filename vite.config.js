9a92127a3e04cf88de16d9d996916897a330df13
Edward Tan <guofoo@gmail.com>
2025-01-24 19:50:08 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/gosim-tailwind/',
+  base: '/gosim-spotlight/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
